a791aa61dcb1094fb6d19c33188ced1bbcd56bfc
Daniel Gonzalez <danilalo321@gmail.com>
2025-01-27 10:45:27 -0500

    Se crea una lista amigos y una funcion para capturar los amigos

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = []; //creamos un array vacio para almacenar los amigos
+
+function agregarAmigo() {
+
+    let amigo=document.getElementById("amigo").value; //obtenemos el valor del input mediente el Id amigo
+    amigos.push(amigo); //agregamos el amigo al array
+}
