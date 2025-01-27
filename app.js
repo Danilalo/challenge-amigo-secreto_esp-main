46d1288d0bdcb386a9d358f23d5e5b535700a5a2
Daniel Gonzalez <danilalo321@gmail.com>
2025-01-27 11:06:16 -0500

    Despues de verificar que los amigos no esten vacio, se anade a un arreglo y se limpia e valor anterior

--- app.js
+++ app.js
@@ -3,14 +3,24 @@
 let amigos = []; //creamos un array vacio para almacenar los amigos
 
 function agregarAmigo() {
-
+    
     let amigo=document.getElementById("amigo").value; //obtenemos el valor del input mediente el Id amigo
+    console.log("es:", amigo);
     if(amigo==""){ //si el input esta vacio
         alert("Ingrese un amigo"); //mostramos un alerta
         return; //retornamos
     }else
     {
         amigos.push(amigo); //agregamos el amigo al array
+        limpiarInput() //limpiamos el input
     }
     
+}
+
+
+
+
+function limpiarInput() { // se crea una función limpiarInput
+    document.getElementById('amigo').value = ''; // se limpia el input
+    return; // se retorna
 }
