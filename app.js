2d495d46079ee1fbcba18192d19bc24613dabd87
Daniel Gonzalez <danilalo321@gmail.com>
2025-01-27 12:19:48 -0500

    Implementa una función para sortear los amigos

--- app.js
+++ app.js
@@ -13,20 +13,34 @@ function agregarAmigo() {
     {
         amigos.push(amigo); //agregamos el amigo al array
         limpiarInput() //limpiamos el input
-        let lista = document.getElementById("listaAmigos"); // Selección por id
-        lista.innerHTML = ""; //limpiamos la lista
+        let lista = document.getElementById("listaAmigos"); // Selección por id de la lista de amigos se hace la asignación a la variable lista desde html
+        lista.innerHTML = ""; //limpiamos la lista 
         for (let i = 0; i < amigos.length; i++) {
             let li = document.createElement("li"); // Crear elemento <li>
             li.textContent = amigos[i]; // Asignar el nombre del amigo como texto
             lista.appendChild(li); // Agregar el <li> a la lista
         }
+        return; //retornamos
+    
 
     }
     
 }
 
+function sortearAmigo(){
+if(amigos.length<2){ //si la cantidad de amigos es menor a 2
+    alert("Ingrese al menos 2 amigos"); //mostramos un alerta
+    return; //retornamos
+}
+else
+{
+let numerorandom=Math.floor(Math.random() * amigos.length); //generamos un numero random
+console.log("numoer:", numerorandom);
+let amigoSecreto=amigos[numerorandom]; //seleccionamos un amigo secreto
+}
 
 
+}
 
 function limpiarInput() { // se crea una función limpiarInput
     document.getElementById('amigo').value = ''; // se limpia el input
